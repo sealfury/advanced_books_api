6e074fae8a096ca0deb6d217d52f8461863fce48
sealfury <willspecker@gmail.com>
2021-01-22 13:39:12 +0100

    creates and generates migration for author associations

---
+++ database/migrations/20210122123237-add_author_id_to_books.js
@@ -0,0 +1,24 @@
+'use strict';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.addColumn(
+      'Books', //source model
+      'authorID', //column name
+      {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Authors', 
+          as: 'author',
+          key: 'id'
+        }
+      }
+    )
+  },
+
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.removeColumn(
+      'Books', 'authorID'
+    )
+  }
+};
